21e31aee21dd14b31fba25cde7b9492220c15873
jwolfe <jwolfe@indeed.com>
2015-06-04 14:31:30 -0700

    Finish de-generifying Query, add Query traversal

--- src/java/com/indeed/jql/language/query/Query.java
+++ src/java/com/indeed/jql/language/query/Query.java
@@ -1,31 +1,34 @@
 package com.indeed.jql.language.query;
 
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import com.indeed.jql.language.AggregateFilter;
 import com.indeed.jql.language.AggregateMetric;
 import com.indeed.jql.language.AggregateMetrics;
 import com.indeed.jql.language.DocFilter;
 import com.indeed.jql.language.DocFilters;
+import com.indeed.jql.language.DocMetric;
 import com.indeed.jql.language.JQLParser;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Query<F, M> {
+public class Query {
     public final List<com.indeed.jql.language.query.Dataset> datasets;
     public final Optional<DocFilter> filter;
     public final List<com.indeed.jql.language.query.GroupBy> groupBys;
-    public final List<M> selects;
+    public final List<AggregateMetric> selects;
 
-    public Query(List<com.indeed.jql.language.query.Dataset> datasets, Optional<DocFilter> filter, List<com.indeed.jql.language.query.GroupBy> groupBys, List<M> selects) {
+    public Query(List<com.indeed.jql.language.query.Dataset> datasets, Optional<DocFilter> filter, List<com.indeed.jql.language.query.GroupBy> groupBys, List<AggregateMetric> selects) {
         this.datasets = datasets;
         this.filter = filter;
         this.groupBys = groupBys;
         this.selects = selects;
     }
 
-    public static Query<AggregateFilter, AggregateMetric> parseQuery(JQLParser.QueryContext queryContext) {
+    public static Query parseQuery(JQLParser.QueryContext queryContext) {
         final List<com.indeed.jql.language.query.Dataset> datasets = com.indeed.jql.language.query.Dataset.parseDatasets(queryContext.fromContents());
 
         final Optional<DocFilter> whereFilter;
@@ -64,6 +67,30 @@ public class Query<F, M> {
             selects = Collections.emptyList();
         }
 
-        return new Query<>(datasets, whereFilter, groupBys, selects);
+        return new Query(datasets, whereFilter, groupBys, selects);
+    }
+
+    public Query traverse(
+            Function<GroupBy, GroupBy> groupBy,
+            Function<AggregateMetric, AggregateMetric> f,
+            Function<DocMetric, DocMetric> g,
+            Function<AggregateFilter, AggregateFilter> h,
+            Function<DocFilter, DocFilter> i
+    ) {
+        final Optional<DocFilter> filter;
+        if (this.filter.isPresent()) {
+            filter = Optional.of(this.filter.get().traverse(g, i));
+        } else {
+            filter = Optional.absent();
+        }
+        final List<GroupBy> groupBys = Lists.newArrayList();
+        for (final GroupBy gb : this.groupBys) {
+            groupBys.add(gb.traverse(groupBy, f, g, h, i));
+        }
+        final List<AggregateMetric> selects = Lists.newArrayList();
+        for (final AggregateMetric select : this.selects) {
+            selects.add(select.traverse(f, g, h, i));
+        }
+        return new Query(datasets, filter, groupBys, selects);
     }
 }
