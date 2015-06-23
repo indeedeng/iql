e8ac5e2c39fbe8669462c150c2647bcbeae7304a
jwolfe <jwolfe@indeed.com>
2015-06-23 12:50:17 -0700

    Turn Not(MetricEquals) into MetricNotEquals, Not(MetricNotEquals) into MetricEquals. Apply filter optimizations before WHERE clause handling

--- src/java/com/indeed/squall/iql2/language/passes/HandleWhereClause.java
+++ src/java/com/indeed/squall/iql2/language/passes/HandleWhereClause.java
@@ -6,6 +6,7 @@ import com.indeed.squall.iql2.language.GroupSuppliers;
 import com.indeed.squall.iql2.language.actions.Action;
 import com.indeed.squall.iql2.language.actions.Actions;
 import com.indeed.squall.iql2.language.execution.ExecutionStep;
+import com.indeed.squall.iql2.language.optimizations.ConstantFolding;
 import com.indeed.squall.iql2.language.query.Query;
 
 import java.util.Collections;
@@ -15,7 +16,7 @@ public class HandleWhereClause {
     public static Result handleWhereClause(Query query) {
         if (query.filter.isPresent()) {
             final Query newQuery = new Query(query.datasets, Optional.<DocFilter>absent(), query.groupBys, query.selects);
-            final List<Action> naiveActions = query.filter.get().getExecutionActions(query.extractDatasetNames(), 1, 1, 0, GroupSuppliers.newGroupSupplier(2));
+            final List<Action> naiveActions = ConstantFolding.apply(query.filter.get()).getExecutionActions(query.extractDatasetNames(), 1, 1, 0, GroupSuppliers.newGroupSupplier(2));
             // TODO: Should the optimization part happen somewhere else?
             final List<Action> optimizedActions = Actions.optimizeConsecutiveQueryActions(naiveActions);
             return new Result(newQuery, Collections.<ExecutionStep>singletonList(new ExecutionStep.FilterActions(optimizedActions)));
