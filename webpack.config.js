4a3149fc1bbfaaa5a51f722a80898b293acb07f8
jwolfe <jwolfe@indeed.com>
2015-10-20 13:29:29 -0700

    Switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+var config = {
+  entry: "./dist/src/Parsers",
+  output: {
+    path: 'dist',
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|gen-src)/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["",".js",".jsx"],
+  },
+  devtool: 'source-map',
+  externals: [
+    'moment',
+    /antlr4(\/.*)?/
+  ]
+};
+
+if (process.env.MINIFY) {
+  config.plugins = [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ];
+}
+
+module.exports = config;
