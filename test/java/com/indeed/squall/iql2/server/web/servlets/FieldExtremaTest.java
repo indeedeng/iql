db63b4947837726e0c821f7330e36bc0b5dc067b
jwolfe <jwolfe@indeed.com>
2016-01-19 17:52:11 +0900

    NOBUG: Add basic test for FIELD_MIN / FIELD_MAX

---
+++ test/java/com/indeed/squall/iql2/server/web/servlets/FieldExtremaTest.java
@@ -0,0 +1,16 @@
+package com.indeed.squall.iql2.server.web.servlets;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FieldExtremaTest {
+    @Test
+    public void basicTest() throws Exception {
+        final List<List<String>> expected = new ArrayList<>();
+        expected.add(ImmutableList.of("", "3", "1000", "0", "15"));
+        QueryServletTestUtils.testIQL2(expected, "from organic yesterday today select FIELD_MIN(oji), FIELD_MAX(oji), FIELD_MIN(ojc), FIELD_MAX(ojc)");
+    }
+}
