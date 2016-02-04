f0897ab320ecba3336d894f453e4de338cdb5cfd
jwolfe <jwolfe@indeed.com>
2016-02-04 17:49:56 +0900

    IQL-204: Add failing test for min/max, fix it

--- test/java/com/indeed/squall/iql2/server/web/servlets/MinMaxTest.java
+++ test/java/com/indeed/squall/iql2/server/web/servlets/MinMaxTest.java
@@ -18,7 +18,7 @@ public class MinMaxTest {
     @Test
     public void testAggregateMinMax() throws Exception {
         final List<List<String>> expected = new ArrayList<>();
-        expected.add(ImmutableList.of("", "0", "-10" ,"1", "7", "2653"));
-        QueryServletTestUtils.testIQL2(OrganicDataset.create(), expected, "from organic yesterday today select min(0,1), min(0,5,7,-10), max(0,1), max(0,5,7,-10), max(oji, ojc)");
+        expected.add(ImmutableList.of("", "0", "-10" ,"1", "7", "2653", "306"));
+        QueryServletTestUtils.testIQL2(OrganicDataset.create(), expected, "from organic yesterday today select min(0,1), min(0,5,7,-10), max(0,1), max(0,5,7,-10), max(oji, ojc), min(oji, ojc)");
     }
 }
