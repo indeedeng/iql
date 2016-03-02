45a60cf4b35ebe1d531cf9440cc5c3cedeb822b3
jwolfe <jwolfe@indeed.com>
2016-03-02 13:52:49 +0900

    IQL-230: Use .stringOrRegroup for IntOrAction against Ramses indexes that aren't declared as metrics

--- src/java/com/indeed/squall/iql2/execution/actions/IntOrAction.java
+++ src/java/com/indeed/squall/iql2/execution/actions/IntOrAction.java
@@ -1,10 +1,14 @@
 package com.indeed.squall.iql2.execution.actions;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.indeed.imhotep.api.ImhotepOutOfMemoryException;
 import com.indeed.squall.iql2.execution.Session;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
 public class IntOrAction implements Action {
@@ -15,6 +19,8 @@ public class IntOrAction implements Action {
     public final int targetGroup;
     public final int positiveGroup;
     public final int negativeGroup;
+
+    private @Nullable Set<String> stringifiedTerms;
 
     public IntOrAction(Set<String> scope, String field, Set<Long> terms, int targetGroup, int positiveGroup, int negativeGroup) {
         this.scope = ImmutableSet.copyOf(scope);
@@ -27,16 +33,35 @@ public class IntOrAction implements Action {
 
     @Override
     public void apply(Session session) throws ImhotepOutOfMemoryException {
-        session.timer.push("sort terms");
-        final long[] terms = new long[this.terms.size()];
-        int i = 0;
-        for (final long term : this.terms) {
-            terms[i] = term;
-            i++;
+        for (final Map.Entry<String, Session.ImhotepSessionInfo> e : session.sessions.entrySet()) {
+            if (scope.contains(e.getKey())) {
+                final Session.ImhotepSessionInfo sessionInfo = e.getValue();
+                if (!sessionInfo.intFields.contains(field)) {
+                    new StringOrAction(Collections.singleton(e.getKey()), field, this.stringifiedTerms(), targetGroup, positiveGroup, negativeGroup).apply(session);
+                } else {
+                    session.timer.push("sort terms");
+                    final long[] terms = new long[this.terms.size()];
+                    int i = 0;
+                    for (final long term : this.terms) {
+                        terms[i] = term;
+                        i++;
+                    }
+                    Arrays.sort(terms);
+                    session.timer.pop();
+                    session.intOrRegroup(field, terms, targetGroup, negativeGroup, positiveGroup, Collections.singleton(field));
+                }
+            }
         }
-        Arrays.sort(terms);
-        session.timer.pop();
-        session.intOrRegroup(field, terms, targetGroup, negativeGroup, positiveGroup, scope);
+    }
+
+    private Set<String> stringifiedTerms() {
+        if (stringifiedTerms == null) {
+            stringifiedTerms = Sets.newHashSetWithExpectedSize(terms.size());
+            for (final long term : terms) {
+                stringifiedTerms.add(String.valueOf(term));
+            }
+        }
+        return stringifiedTerms;
     }
 
     @Override
