befd9c9c5bb6763a8bab04eae3880a63cc4e0af0
jwolfe <jwolfe@indeed.com>
2016-03-02 19:11:28 +0900

    IQL-206: Add test for number formatting

---
+++ test/java/com/indeed/squall/iql2/server/web/servlets/PrintfTest.java
@@ -0,0 +1,30 @@
+package com.indeed.squall.iql2.server.web.servlets;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testAll;
+
+public class PrintfTest {
+    @Test
+    public void testGetGroupStats() throws Exception {
+        // More convoluted than printf(1/3, '%0.2d') to support both IQL1 and IQL2.
+        // In IQL1, 1/3 == count() / 3
+        final List<List<String>> expected = new ArrayList<>();
+        expected.add(ImmutableList.of("a", "0.33"));
+        expected.add(ImmutableList.of("b", "0.33"));
+        expected.add(ImmutableList.of("c", "0.33"));
+        expected.add(ImmutableList.of("d", "0.33"));
+        testAll(OrganicDataset.create(), expected, "from organic yesterday today group by tk select printf(count()/(3*count()), '%.2f')");
+    }
+
+    @Test
+    public void testWithGroupBy() throws Exception {
+        // More convoluted than printf(1/3, '%0.2d') to support both IQL1 and IQL2.
+        // In IQL1, 1/3 == count() / 3
+        testAll(OrganicDataset.create(), ImmutableList.<List<String>>of(ImmutableList.of("", "0.33")), "from organic yesterday today select printf(count()/(3*count()), '%.2f')");
+    }
+}
