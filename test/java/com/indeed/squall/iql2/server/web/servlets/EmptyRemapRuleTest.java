08b9c1b209ae9787cc6e721adbb839a1a95b8e87
jwolfe <jwolfe@indeed.com>
2016-05-17 14:44:26 +0900

    IQL-249: Add failing unit test for empty condition GroupMultiRemapRules

---
+++ test/java/com/indeed/squall/iql2/server/web/servlets/EmptyRemapRuleTest.java
@@ -0,0 +1,17 @@
+package com.indeed.squall.iql2.server.web.servlets;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmptyRemapRuleTest {
+    @Test
+    public void basicTest() throws Exception {
+        final List<List<String>> expected = new ArrayList<>();
+        expected.add(ImmutableList.of("DEFAULT", "151"));
+        QueryServletTestUtils.testIQL2(OrganicDataset.create(), expected, "from organic yesterday today group by oji[having count() > 200] with default");
+    }
+
+}
