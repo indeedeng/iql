0c7c9a88ea329906b7be783f06a9fdb8a5fe938b
Vladimir Markin <vladimir@indeed.com>
2016-08-10 01:37:44 -0500

    ImhotepErrorResolver and exception classes moved to imhotep-client

--- src/main/java/com/indeed/imhotep/metadata/FieldMetadata.java
+++ src/main/java/com/indeed/imhotep/metadata/FieldMetadata.java
@@ -33,8 +33,8 @@ public class FieldMetadata {
 
     public FieldMetadata(@Nonnull String name, @Nonnull FieldType imhotepType) {
         this.name = name;
-        this.imhotepType = imhotepType;
-        this.type = imhotepType;
+        this.imhotepType = FieldType.Integer; //imhotepType;
+        this.type = FieldType.Integer; //imhotepType;
     }
 
     @Nonnull
