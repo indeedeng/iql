d2f2c5288acfed17d56a71b579664d06d0ed43c0
jwolfe <jwolfe@indeed.com>
2017-03-14 18:14:05 +0900

    NOBUG: Clean up unused constructor

--- src/java/com/indeed/squall/iql2/language/query/SplitQuery.java
+++ src/java/com/indeed/squall/iql2/language/query/SplitQuery.java
@@ -18,23 +18,6 @@ public class SplitQuery {
     public final String end;
     public final String endRawString;
 
-    public SplitQuery(String from, String where, String groupBy, String select, String limit, List<String> groupBys, List<String> selects) {
-        this.from = from;
-        this.where = where;
-        this.groupBy = groupBy;
-        this.select = select;
-        this.limit = limit;
-
-        this.groupBys = groupBys;
-        this.selects = selects;
-
-        this.dataset = "";
-        this.start = "";
-        this.startRawString = "";
-        this.end = "";
-        this.endRawString = "";
-    }
-
     public SplitQuery(String from, String where, String groupBy, String select, String limit, List<String> groupBys, List<String> selects, String dataset, String start, String startRawString, String end, String endRawString) {
         this.from = from;
         this.where = where;
