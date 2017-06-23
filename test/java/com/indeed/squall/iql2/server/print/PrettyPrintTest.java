00b1412094f35dfc0e29f9cf79e29b72e662ff65
ted.zheli <zheli@indeed.com>
2017-06-23 12:40:32 +0900

    hotfix: fix empty ParserRuleContext for nodes created internally

--- test/java/com/indeed/squall/iql2/server/print/PrettyPrintTest.java
+++ test/java/com/indeed/squall/iql2/server/print/PrettyPrintTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 public class PrettyPrintTest {
     @Test
     public void prettyPrint() throws Exception {
+        Assert.assertEquals("FROM jobsearch 1d 0d\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch w2d t"));
         Assert.assertEquals("FROM jobsearch 2d 1d\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch 2d 1d select count()"));
         Assert.assertEquals("FROM jobsearch /* hi */ 2d 1d\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch /* hi */ 2d 1d select count()"));
         Assert.assertEquals("FROM jobsearch 2d 1d\nSELECT /*before*/ count() /* AFTER */", PrettyPrint.prettyPrint("from jobsearch 2d 1d select /*before*/count()/* AFTER */"));
@@ -20,6 +21,7 @@ public class PrettyPrintTest {
         Assert.assertEquals("FROM jobsearch yesterday today\nWHERE (country=\"us\") (oji=10)\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch yesterday today where country:\"us\" oji:10 select count()"));
         Assert.assertEquals("FROM jobsearch yesterday today\nWHERE (ctk=~\".*\")\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch yesterday today where ctk=~\".*\" select count()"));
         Assert.assertEquals("FROM jobsearch yesterday today\nWHERE (escaped=\"stuff\\\"\")\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch yesterday today where escaped='stuff\"' select count()"));
+        Assert.assertEquals("FROM jobsearch 2d 1d\nWHERE (not(country=\"us\")) (not(country=\"us\"))\nSELECT count()", PrettyPrint.prettyPrint("from jobsearch 2d 1d WHERE -country:us and -country=us"));
     }
 
     @Test
