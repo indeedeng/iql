978bcbdc4e751342514216f1542a1d8d3cd0c606
ted.zheli <zheli@indeed.com>
2017-06-27 10:21:06 +0900

    IQL-440: don't allow dash in non backquote id

--- src/java/com/indeed/squall/iql2/language/Identifiers.java
+++ src/java/com/indeed/squall/iql2/language/Identifiers.java
@@ -3,7 +3,7 @@ package com.indeed.squall.iql2.language;
 import java.util.regex.Pattern;
 
 public class Identifiers {
-    private static final Pattern IDENTIFIER_PATTERN = Pattern.compile("[a-zA-Z_][a-zA-Z0-9_-]*");
+    private static final Pattern IDENTIFIER_PATTERN = Pattern.compile("[a-zA-Z_][a-zA-Z0-9_]*");
 
     public static Positioned<String> parseIdentifier(JQLParser.IdentifierContext identifierContext) {
         final String result;
@@ -11,9 +11,9 @@ public class Identifiers {
             result = identifierContext.getText().substring(1, identifierContext.getText().length() - 1).toUpperCase();
         } else {
             result = identifierContext.getText().toUpperCase();
-        }
-        if (!result.isEmpty() && !IDENTIFIER_PATTERN.matcher(result).matches()) {
-            throw new IllegalArgumentException("identifier " + result +" doesn't match pattern : " + IDENTIFIER_PATTERN.toString());
+            if (!IDENTIFIER_PATTERN.matcher(result).matches()) {
+                throw new IllegalArgumentException("identifier " + result +" doesn't match pattern : " + IDENTIFIER_PATTERN.toString());
+            }
         }
         return Positioned.from(result, identifierContext);
     }
