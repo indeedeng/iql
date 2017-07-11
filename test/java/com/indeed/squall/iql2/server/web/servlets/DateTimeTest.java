1a36a9bc6521c0b8e74d2f6c219c7248389d3841
zheli <zheli@indeed.com>
2017-07-11 11:00:37 -0500

    IQL-447: add more datetime tests

--- test/java/com/indeed/squall/iql2/server/web/servlets/DateTimeTest.java
+++ test/java/com/indeed/squall/iql2/server/web/servlets/DateTimeTest.java
@@ -4,14 +4,19 @@ import com.google.common.collect.ImmutableList;
 import com.indeed.squall.iql2.server.web.servlets.dataset.OrganicDataset;
 import org.junit.Test;
 
+import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testAll;
 import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testIQL1;
+import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testIQL2;
 
 public class DateTimeTest extends BasicTest {
     @Test
     public void testWordDate() throws Exception {
         testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "151", "2653", "306")), "from organic y t select count(), oji, ojc");
-        testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "151", "2653", "306")), "from organic 3days ago select count(), oji, ojc");
-        testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "1", "23", "1")), "from organic 60minute ago select count(), oji, ojc");
-        testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "151", "2653", "306")), "from organic d ago select count(), oji, ojc");
+        testAll(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "153", "2655", "308")), "from organic 3days ago select count(), oji, ojc");
+        testAll(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "1", "23", "1")), "from organic 60minute ago select count(), oji, ojc");
+        testAll(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "151", "2653", "306")), "from organic d ago select count(), oji, ojc");
+        testAll(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "1", "23", "1")), "from organic 60minute ago select count(), oji, ojc");
+        testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "1", "23", "1")), "from organic 60M ago select count(), oji, ojc");
+        testIQL2(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "182", "2684", "337")), "from organic 60M ago select count(), oji, ojc");
     }
 }
