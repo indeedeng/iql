dc7861931835d6a3c9d16d9ece6ada22a8f38cda
Zhe Li <zheli@indeed.com>
2017-07-18 12:51:05 +0900

    IQL-202: add tests for filter in multi datasets

--- test/java/com/indeed/squall/iql2/server/web/servlets/BasicFilterTest.java
+++ test/java/com/indeed/squall/iql2/server/web/servlets/BasicFilterTest.java
@@ -1,6 +1,7 @@
 package com.indeed.squall.iql2.server.web.servlets;
 
 import com.google.common.collect.ImmutableList;
+import com.indeed.squall.iql2.server.web.servlets.dataset.MultipleDataset;
 import com.indeed.squall.iql2.server.web.servlets.dataset.OrganicDataset;
 import org.junit.Test;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testAll;
 import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testIQL1;
+import static com.indeed.squall.iql2.server.web.servlets.QueryServletTestUtils.testIQL2;
 
 public class BasicFilterTest extends BasicTest {
     @Test
@@ -17,6 +19,13 @@ public class BasicFilterTest extends BasicTest {
         testAll(OrganicDataset.create(), ImmutableList.<List<String>>of(ImmutableList.of("", "4")), "from organic yesterday today where tk=\"c\" select count()");
         testAll(OrganicDataset.create(), ImmutableList.<List<String>>of(ImmutableList.of("", "141")), "from organic yesterday today where tk=\"d\" select count()");
         testIQL1(OrganicDataset.create(), ImmutableList.of(ImmutableList.of("", "141")), "from organic yesterday today where tk:d select count()");
+    }
+
+    @Test
+    public void testMultipleDatasetFilters() throws Exception {
+        testIQL2(MultipleDataset.create(), ImmutableList.of(ImmutableList.of("", "2")), "from dataset1 yesterday today, dataset2 where intField1=1 select count()");
+        testIQL2(MultipleDataset.create(), ImmutableList.of(ImmutableList.of("", "22")), "from dataset1 1month today, dataset2 where strField1='1' select count()");
+        testIQL2(MultipleDataset.create(), ImmutableList.of(ImmutableList.of("", "3")), "from dataset1 1w today, dataset2 where dataset1.intField2=1 select dataset1.count()");
     }
 
     @Test
