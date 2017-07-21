66b8832ccdcbbdcbd6a143bac1315406ee5d8f1b
Vladimir Markin <vladimir@indeed.com>
2017-07-21 12:25:31 -0500

    IQL-82: MySQL 5.7 compatibility

--- db/updates/update-iqldb.sql
+++ db/updates/update-iqldb.sql
@@ -4,8 +4,8 @@ CREATE TABLE `tblrunning` (
   `qhash` varchar(30) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL COMMENT 'Hash of the query for duplicate detection',
   `username` varchar(100) COLLATE utf8_bin NOT NULL COMMENT 'Username of the user executing the query',
   `client` varchar(100) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL COMMENT 'Name of the IQL client application that started the query',
-  `submit_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time the query execution was requested by the user',
-  `execution_start_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time the query execution actually started',
+  `submit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time the query execution was requested by the user',
+  `execution_start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time the query execution actually started',
   `hostname` varchar(20) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL COMMENT 'Hostname of the IQL daemon executing the query',
   `sessions` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'Number of Imhotep sessions used by this query',
   `killed` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1 if query was requested to be stopped'
