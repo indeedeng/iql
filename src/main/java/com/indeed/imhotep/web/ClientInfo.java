ac4fa72329053b242dde43e1a27d3b57fa2633e5
Vladimir Markin <vladimir@indeed.com>
2017-08-17 14:19:28 -0500

    ISHBOOK-1047: log additional client information if passed in: author, clientProcessId, clientProcessName, clientExecutionId

--- src/main/java/com/indeed/imhotep/web/ClientInfo.java
+++ src/main/java/com/indeed/imhotep/web/ClientInfo.java
@@ -3,19 +3,33 @@ package com.indeed.imhotep.web;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ClientInfo {
+    /** name of the user performing the query */
     public final String username;
+    /** name of the user who composed the query */
+    @JsonIgnore
+    public final String author;
+    /** name of the application performing the query */
     public final String client;
+    /** unique id of the internal process happening in the client */
+    @JsonIgnore
+    public final String clientProcessId;
+    /** string name of the internal process happening in the client */
+    public final String clientProcessName;
+    /** unique id of the internal process execution in the client */
+    @JsonIgnore
+    public final String clientExecutionId;
+
+    /** Whether this client allows multiple users to run queries */
     @JsonIgnore
     public final boolean isMultiuserClient;
-    // TODO: add more fields
-//    public final String author;
-//    public final String clientExecutionId;
-//    public final String clientId;
 
-
-    public ClientInfo(String username, String client, boolean isMultiuserClient) {
+    public ClientInfo(String username, String author, String client, String clientProcessId, String clientProcessName, String clientExecutionId, boolean isMultiuserClient) {
         this.username = username;
+        this.author = author;
         this.client = client;
+        this.clientProcessId = clientProcessId;
+        this.clientProcessName = clientProcessName;
+        this.clientExecutionId = clientExecutionId;
         this.isMultiuserClient = isMultiuserClient;
     }
 }
