eccfa5f344d7b5f5435215bb239e1cbd082b514d
Vladimir Markin <vladimir@indeed.com>
2017-08-24 19:00:38 -0500

    Return to the client a few extra performance metrics in the IQL-Timings header: lockWaitMillis, cacheCheckMillis, shardsSelectionMillis.

--- src/main/java/com/indeed/imhotep/iql/SelectExecutionStats.java
+++ src/main/java/com/indeed/imhotep/iql/SelectExecutionStats.java
@@ -1,7 +1,10 @@
 package com.indeed.imhotep.iql;
 
+import com.google.common.collect.Sets;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongArrayMap;
+
+import java.util.Set;
 
 public class SelectExecutionStats {
     public boolean cached;
@@ -15,6 +18,8 @@ public class SelectExecutionStats {
     public int maxImhotepGroups;
     public Object2LongMap<String> phases = new Object2LongArrayMap<String>();
     public long numDocs;
+
+    private final Set<String> phasesToReport = Sets.newHashSet("lockWaitMillis", "cacheCheckMillis", "shardsSelectionMillis");
 
     public SelectExecutionStats() {
         this.headOnly = false;
@@ -31,5 +36,19 @@ public class SelectExecutionStats {
 
     public void setPhase(final String phase, long elapsedtimemillis) {
         phases.put(phase, elapsedtimemillis);
+    }
+
+    public String getPhasesAsTimingReport() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (Object2LongMap.Entry<String> entry : phases.object2LongEntrySet()) {
+            if(!phasesToReport.contains(entry.getKey())) {
+                continue;
+            }
+            stringBuilder.append(entry.getLongValue());
+            stringBuilder.append("ms ");
+            stringBuilder.append(entry.getKey());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
