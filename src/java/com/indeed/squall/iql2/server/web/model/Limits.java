df0821f8d490f2de6d9c64d68bb9f16ae7aa284c
Yuan Lei <yuanlei@indeed.com>
2017-10-31 14:06:08 +0900

    IQL-463: add user related models

---
+++ src/java/com/indeed/squall/iql2/server/web/model/Limits.java
@@ -0,0 +1,64 @@
+package com.indeed.squall.iql2.server.web.model;
+import com.indeed.imhotep.exceptions.GroupLimitExceededException;
+
+import javax.annotation.Nullable;
+import java.text.DecimalFormat;
+
+/**
+ * @author yuanlei
+ */
+
+public class Limits {
+    @Nullable public final Integer queryDocumentCountLimitBillions;
+    @Nullable public final Integer queryInMemoryRowsLimit;
+    @Nullable public final Integer queryFTGSIQLLimitMB;
+    @Nullable public final Integer queryFTGSImhotepDaemonLimitMB;
+    @Nullable public final Integer concurrentQueriesLimit;
+    @Nullable public final Integer concurrentImhotepSessionsLimit;
+
+    public Limits(Integer queryDocumentCountLimitBillions, Integer queryInMemoryRowsLimit, Integer queryFTGSIQLLimitMB, Integer queryFTGSImhotepDaemonLimitMB, Integer concurrentQueriesLimit, Integer concurrentImhotepSessionsLimit) {
+        this.queryDocumentCountLimitBillions = queryDocumentCountLimitBillions;
+        this.queryInMemoryRowsLimit = queryInMemoryRowsLimit;
+        this.queryFTGSIQLLimitMB = queryFTGSIQLLimitMB;
+        this.queryFTGSImhotepDaemonLimitMB = queryFTGSImhotepDaemonLimitMB;
+        this.concurrentQueriesLimit = concurrentQueriesLimit;
+        this.concurrentImhotepSessionsLimit = concurrentImhotepSessionsLimit;
+    }
+
+    public boolean satisfiesQueryDocumentCountLimit(long value) {
+        return queryDocumentCountLimitBillions == null || value <= (long)queryDocumentCountLimitBillions * 1_000_000_000;
+    }
+
+    public boolean satisfiesQueryInMemoryRowsLimit(double value) {
+        return queryInMemoryRowsLimit == null || value <= queryInMemoryRowsLimit;
+    }
+
+    public void assertQueryInMemoryRowsLimit(double newNumGroups) {
+        if(!satisfiesQueryInMemoryRowsLimit(newNumGroups)) {
+            DecimalFormat df = new DecimalFormat("###,###");
+            throw new GroupLimitExceededException("Number of groups " + df.format(newNumGroups) + " exceeds the limit " + df.format(queryInMemoryRowsLimit)+
+                    ". Please simplify the query.");
+        }
+    }
+
+    public boolean satisfiesQueryInMemoryRowsLimit(int value) {
+        return queryInMemoryRowsLimit == null || value <= queryInMemoryRowsLimit;
+    }
+
+    public boolean satisfiesQueryFTGSIQLLimitMB(int value) {
+        return queryFTGSIQLLimitMB == null || value <= queryFTGSIQLLimitMB;
+    }
+
+    public boolean satisfiesQueryFTGSImhotepDaemonLimitMB(int value) {
+        return queryFTGSImhotepDaemonLimitMB == null || value <= queryFTGSImhotepDaemonLimitMB;
+    }
+
+    public boolean satisfiesConcurrentQueriesLimit(int value) {
+        return concurrentQueriesLimit == null || value <= concurrentQueriesLimit;
+    }
+
+    public boolean satisfiesConcurrentImhotepSessionsLimit(int value) {
+        return concurrentImhotepSessionsLimit == null || value <= concurrentImhotepSessionsLimit;
+    }
+}
+
---
+++ src/java/com/indeed/squall/iql2/server/web/model/RunningQuery.java
@@ -0,0 +1,107 @@
+package com.indeed.squall.iql2.server.web.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.joda.time.DateTime;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yuanlei
+ */
+public class RunningQuery {
+    public final long id;
+    public final String query;
+    public final String qHash;
+    public final String username;
+    public final String client;
+    public final DateTime submitTime;
+    public final DateTime startedTime;
+    public final String hostname;
+    @JsonIgnore
+    public final byte sessions;
+    @JsonIgnore
+    public final boolean killed;
+
+    public RunningQuery(long id, String query, String qHash, String username, String client, DateTime submitTime, DateTime startedTime, String hostname, byte sessions, boolean killed) {
+        this.id = id;
+        this.query = query;
+        this.qHash = qHash;
+        this.username = username;
+        this.client = client;
+        this.submitTime = submitTime;
+        this.startedTime = startedTime;
+        this.hostname = hostname;
+        this.sessions = sessions;
+        this.killed = killed;
+    }
+
+    public String getSubmitTime() {
+        return submitTime.toString();
+    }
+
+    public String getStartedTime() {
+        return startedTime != null ? startedTime.toString() : null;
+    }
+
+    @Override
+    public String toString() {
+        return "RunningQuery{\n" +
+                "\n id=" + id +
+                "\n qHash=" + qHash +
+                "\n username=" + username +
+                "\n client=" + client +
+                "\n submitTime=" + submitTime +
+                "\n startedTime=" + startedTime +
+                "\n hostname=" + hostname +
+                "\n sessions=" + sessions +
+                "\n killed=" + killed +
+                "\n query=" + query +
+                "\n}";
+    }
+
+    public static RowMapper<RunningQuery> resultSetRowMapper = new RowMapper<RunningQuery>() {
+        @Override
+        public RunningQuery mapRow(ResultSet rs, int rowNum) throws SQLException {
+            final Timestamp submitTime = rs.getTimestamp("submit_time");
+            Timestamp executionStartTime = rs.getTimestamp("execution_start_time");
+            if(executionStartTime.getTime() < TimeUnit.DAYS.toMillis(2)) {
+                executionStartTime = null;  // Hack to workaround the column not allowing nulls
+            }
+
+            return new RunningQuery(
+                    rs.getLong("id"),
+                    rs.getString("query"),
+                    rs.getString("qhash"),
+                    rs.getString("username"),
+                    rs.getString("client"),
+                    submitTime != null ? new DateTime(submitTime.getTime()) : null,
+                    executionStartTime != null ? new DateTime(executionStartTime.getTime()) : null,
+                    rs.getString("hostname"),
+                    rs.getByte("sessions"),
+                    rs.getBoolean("killed"));
+        }
+    };
+
+    public static RowMapper<RunningQuery> resultSetRowMapperForLocking = new RowMapper<RunningQuery>() {
+        @Override
+        public RunningQuery mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+            return new RunningQuery(
+                    rs.getLong("id"),
+                    null,
+                    rs.getString("qhash"),
+                    rs.getString("username"),
+                    rs.getString("client"),
+                    null,
+                    null,
+                    null,
+                    rs.getByte("sessions"),
+                    rs.getBoolean("killed"));
+        }
+    };
+}
+
