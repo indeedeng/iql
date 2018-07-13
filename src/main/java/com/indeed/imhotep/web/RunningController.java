bda8e36a88a85f4453b3b8134cf23b653fdc573d
Vladimir Markin <vladimir@indeed.com>
2018-07-13 18:43:43 -0500

    Don't throw errors in /allrunning when iqldb is disabled e.g. in dev

--- src/main/java/com/indeed/imhotep/web/RunningController.java
+++ src/main/java/com/indeed/imhotep/web/RunningController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -78,6 +79,9 @@ public class RunningController {
     @RequestMapping("/allrunning")
     @ResponseBody
     public RunningQueriesState handleAllRunning() {
+        if (iqldb == null) {
+            return new RunningQueriesState(new ArrayList<>());
+        }
         return new RunningQueriesState(iqldb.getRunningQueries());
     }
 
