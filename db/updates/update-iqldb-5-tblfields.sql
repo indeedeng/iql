b25a01be77f56ec5c864634b7a68192b5b0556a2
Vladimir Markin <vladimir@indeed.com>
2018-08-24 19:08:43 -0500

    IMTEPD--362: deleted SQL table definitions since they belong in Imhotep

--- db/updates/update-iqldb-5-tblfields.sql
+++
@@ -1,8 +0,0 @@
-create table tblfields
-(
-  dataset            varchar(100)           not null,
-  fieldname          varchar(256)           not null,
-  type               enum ('INT', 'STRING') not null,
-  lastshardstarttime bigint                 not null,
-  primary key (dataset, fieldname, type)
-);
