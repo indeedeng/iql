69f345648d08653bf4220d3f820929a9a1f8d31a
Xueping Weng <xweng@indeed.com>
2019-01-15 18:25:43 +0900

    IQL-606: remove stats count() for subquery

--- src/main/java/com/indeed/iql2/language/execution/passes/OptimizeLast.java
+++ src/main/java/com/indeed/iql2/language/execution/passes/OptimizeLast.java
@@ -37,72 +37,65 @@ import java.util.Set;
 public class OptimizeLast {
     public static List<ExecutionStep> optimize(List<ExecutionStep> steps, Optional<Integer> queryLimit) {
         steps = Collections.unmodifiableList(steps);
-        if (steps.size() > 1) {
+        if (steps.size() >= 1) {
+            // IQL-606: translate the last ExplodeAndRegroup(without GetGroupStats following) into IterateStats
             final ExecutionStep last = steps.get(steps.size() - 1);
-            final ExecutionStep penultimate = steps.get(steps.size() - 2);
-            if (last instanceof ExecutionStep.GetGroupStats
-                    && penultimate instanceof ExecutionStep.ExplodeAndRegroup) {
-                final ExecutionStep.GetGroupStats getGroupStats = (ExecutionStep.GetGroupStats) last;
-                final boolean selectIsOrdered = Iterables.any(getGroupStats.stats, new Predicate<AggregateMetric>() {
-                    @Override
-                    public boolean apply(AggregateMetric input) {
-                        return input.isOrdered();
-                    }
-                });
-                final ExecutionStep.ExplodeAndRegroup explodeAndRegroup = (ExecutionStep.ExplodeAndRegroup) penultimate;
-                // TODO: Make query execution sort on .metric whether or not there's a limit, make .metric optional. Then change this to care if .metric.isPresent() also.
-                // TODO: Figure out wtf the above TODO means.
-                final boolean hasDefault = explodeAndRegroup.withDefault;
-                if (!selectIsOrdered && !hasDefault) { // If there's a filter and something that depends on order, we can't merge them.
-                    final List<ExecutionStep> newSteps = new ArrayList<>();
-                    newSteps.addAll(steps);
-                    newSteps.remove(newSteps.size() - 1);
-                    newSteps.remove(newSteps.size() - 1);
-                    newSteps.add(new ExecutionStep.IterateStats(
-                            explodeAndRegroup.field,
-                            explodeAndRegroup.filter,
-                            explodeAndRegroup.limit,
-                            queryLimit,
-                            explodeAndRegroup.metric,
-                            Optional.<Set<String>>absent(),
-                            Optional.<Set<Long>>absent(),
-                            fixForIteration(getGroupStats.stats),
-                            getGroupStats.formatStrings
-                            ));
-                    return newSteps;
-                }
-            } else if (last instanceof ExecutionStep.GetGroupStats && penultimate instanceof ExecutionStep.ExplodeFieldIn) {
-                final ExecutionStep.ExplodeFieldIn explodeFieldIn = (ExecutionStep.ExplodeFieldIn) penultimate;
-                final ExecutionStep.GetGroupStats getGroupStats = (ExecutionStep.GetGroupStats) last;
-                if (!explodeFieldIn.withDefault) {
-                    final Optional<Set<Long>> intTermSubset;
-                    if (!explodeFieldIn.intTerms.isEmpty()) {
-                        intTermSubset = Optional.<Set<Long>>of(new LongAVLTreeSet(explodeFieldIn.intTerms));
-                    } else {
-                        intTermSubset = Optional.absent();
-                    }
+            if (last instanceof ExecutionStep.ExplodeAndRegroup) {
+                return optimizeExplodeAndRegroup(
+                        steps,
+                        (ExecutionStep.ExplodeAndRegroup) last,
+                        Collections.emptyList(),
+                        Collections.emptyList(),
+                        queryLimit,
+                        1);
+            }
 
-                    final Optional<Set<String>> stringTermSubset;
-                    if (!explodeFieldIn.stringTerms.isEmpty()) {
-                        stringTermSubset = Optional.<Set<String>>of(Sets.newTreeSet(explodeFieldIn.stringTerms));
-                    } else {
-                        stringTermSubset = Optional.absent();
-                    }
+            if (steps.size() > 1) {
+                final ExecutionStep penultimate = steps.get(steps.size() - 2);
 
-                    final List<ExecutionStep> newSteps = new ArrayList<>();
-                    newSteps.addAll(steps.subList(0, steps.size() - 2));
-                    newSteps.add(new ExecutionStep.IterateStats(
-                            explodeFieldIn.field,
-                            Optional.<AggregateFilter>absent(),
-                            Optional.<Long>absent(),
+                if (last instanceof ExecutionStep.GetGroupStats
+                        && penultimate instanceof ExecutionStep.ExplodeAndRegroup) {
+                    final ExecutionStep.GetGroupStats getGroupStats = (ExecutionStep.GetGroupStats) last;
+                    return optimizeExplodeAndRegroup(
+                            steps,
+                            (ExecutionStep.ExplodeAndRegroup) penultimate,
+                            getGroupStats.stats,
+                            getGroupStats.formatStrings,
                             queryLimit,
-                            Optional.<AggregateMetric>absent(),
-                            stringTermSubset,
-                            intTermSubset,
-                            fixForIteration(getGroupStats.stats),
-                            getGroupStats.formatStrings
-                    ));
-                    return newSteps;
+                            2);
+                } else if (last instanceof ExecutionStep.GetGroupStats && penultimate instanceof ExecutionStep.ExplodeFieldIn) {
+                    final ExecutionStep.ExplodeFieldIn explodeFieldIn = (ExecutionStep.ExplodeFieldIn) penultimate;
+                    final ExecutionStep.GetGroupStats getGroupStats = (ExecutionStep.GetGroupStats) last;
+                    if (!explodeFieldIn.withDefault) {
+                        final Optional<Set<Long>> intTermSubset;
+                        if (!explodeFieldIn.intTerms.isEmpty()) {
+                            intTermSubset = Optional.<Set<Long>>of(new LongAVLTreeSet(explodeFieldIn.intTerms));
+                        } else {
+                            intTermSubset = Optional.absent();
+                        }
+
+                        final Optional<Set<String>> stringTermSubset;
+                        if (!explodeFieldIn.stringTerms.isEmpty()) {
+                            stringTermSubset = Optional.<Set<String>>of(Sets.newTreeSet(explodeFieldIn.stringTerms));
+                        } else {
+                            stringTermSubset = Optional.absent();
+                        }
+
+                        final List<ExecutionStep> newSteps = new ArrayList<>();
+                        newSteps.addAll(steps.subList(0, steps.size() - 2));
+                        newSteps.add(new ExecutionStep.IterateStats(
+                                explodeFieldIn.field,
+                                Optional.<AggregateFilter>absent(),
+                                Optional.<Long>absent(),
+                                queryLimit,
+                                Optional.<AggregateMetric>absent(),
+                                stringTermSubset,
+                                intTermSubset,
+                                fixForIteration(getGroupStats.stats),
+                                getGroupStats.formatStrings
+                        ));
+                        return newSteps;
+                    }
                 }
             }
         }
@@ -121,6 +114,44 @@ public class OptimizeLast {
             ));
         }
         return result;
+    }
+
+    private static final List<ExecutionStep> optimizeExplodeAndRegroup(
+            final List<ExecutionStep> steps,
+            final ExecutionStep.ExplodeAndRegroup explodeAndRegroup,
+            final List<AggregateMetric> metricsInGetGroupStats,
+            final List<Optional<String>> formatStringsInGetGroupStats,
+            final Optional<Integer> queryLimit,
+            final int stepsToRemove) {
+        final boolean selectIsOrdered = Iterables.any(metricsInGetGroupStats, new Predicate<AggregateMetric>() {
+            @Override
+            public boolean apply(AggregateMetric input) {
+                return input.isOrdered();
+            }
+        });
+        // TODO: Make query execution sort on .metric whether or not there's a limit, make .metric optional. Then change this to care if .metric.isPresent() also.
+        // TODO: Figure out wtf the above TODO means.
+        final boolean hasDefault = explodeAndRegroup.withDefault;
+        if (!selectIsOrdered && !hasDefault) { // If there's a filter and something that depends on order, we can't merge them.
+            List<ExecutionStep> newSteps = new ArrayList<>(steps);
+            int i = 0;
+            while (i++ < stepsToRemove) {
+                newSteps.remove(newSteps.size() -1);
+            }
+            newSteps.add(new ExecutionStep.IterateStats(
+                    explodeAndRegroup.field,
+                    explodeAndRegroup.filter,
+                    explodeAndRegroup.limit,
+                    queryLimit,
+                    explodeAndRegroup.metric,
+                    Optional.<Set<String>>absent(),
+                    Optional.<Set<Long>>absent(),
+                    fixForIteration(metricsInGetGroupStats),
+                    formatStringsInGetGroupStats
+            ));
+            return newSteps;
+        }
+        return steps;
     }
 
     private static final Function<AggregateMetric,AggregateMetric> PROCESS_METRIC = new Function<AggregateMetric, AggregateMetric>() {
