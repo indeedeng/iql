8254d7f9034cb55d8b1819c8d9bdba094f9c51ce
Michihiko Sawa <michihiko@indeed.com>
2019-03-20 14:29:14 +0900

    IQL-864: Add tests that fails with hash collision

---
+++ src/test/java/com/indeed/iql2/server/web/servlets/SimilarQueryTest.java
@@ -0,0 +1,47 @@
+package com.indeed.iql2.server.web.servlets;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import static com.indeed.iql2.server.web.servlets.QueryServletTestUtils.testAll;
+
+/**
+ * These are supposed to be executed
+ */
+public class SimilarQueryTest {
+    @Test
+    public void testStringFieldIn() throws Exception {
+        testAll(
+                ImmutableList.of(ImmutableList.of("", "2")),
+                "from countries yesterday today where country in ('au', 'co')"
+        );
+        testAll(
+                ImmutableList.of(ImmutableList.of("", "0")),
+                "from countries yesterday today where country in ('au, co')"
+        );
+    }
+
+    @Test
+    public void testSubsetFTGS() throws Exception {
+        testAll(
+                ImmutableList.of(ImmutableList.of("au", "1"), ImmutableList.of("co", "1")),
+                "from countries yesterday today group by country in ('au', 'co')"
+        );
+        testAll(
+                ImmutableList.of(),
+                "from countries yesterday today group by country in ('au, co')"
+        );
+    }
+
+    @Test
+    public void testGroupByFieldIn() throws Exception {
+        testAll(
+                ImmutableList.of(ImmutableList.of("au", "[1, 2)", "1"), ImmutableList.of("co", "[1, 2)", "1")),
+                "from countries yesterday today group by country in ('au', 'co'), bucket(count(), 1, 2, 1, 1)"
+        );
+        testAll(
+                ImmutableList.of(),
+                "from countries yesterday today group by country in ('au, co'), bucket(count(), 1, 2, 1, 1)"
+        );
+    }
+}
