685ae866f384a88c59c32039ccbedbb7e16a4ec3
Raj Manvar <rmanvar@indeed.com>
2019-03-28 09:23:55 +0900

    IMTEPD-493: Undo temporary testUtil change

--- src/test/java/com/indeed/iql2/server/web/servlets/QueryServletTestUtils.java
+++ src/test/java/com/indeed/iql2/server/web/servlets/QueryServletTestUtils.java
@@ -86,6 +86,7 @@ public class QueryServletTestUtils extends BasicTest {
             OPTIONS_TO_TEST = ImmutableList.of(ImmutableSet.of());
         } else {
             OPTIONS_TO_TEST = ImmutableList.of(
+                    ImmutableSet.of(),
                     ImmutableSet.of(QueryOptions.PARANOID),
                     ImmutableSet.of(QueryOptions.Experimental.ASYNC),
                     ImmutableSet.of(QueryOptions.Experimental.ASYNC, QueryOptions.PARANOID),
